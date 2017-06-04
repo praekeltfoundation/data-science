ab09497806099717b38c7c18df244ec389bfbd65
charles <charles@Charles-MacBook-Pro.local>
2017-06-04 10:36:22 +0200

    switched to psycopg2 for sql interfacing-- more robust

--- gates_sampler/gates-sampler.py
+++ gates_sampler/gates-sampler.py
@@ -6,6 +6,8 @@ from bokeh.io import output_file, show
 from bokeh.models import GeoJSONDataSource
 from bokeh.plotting import figure
 from bokeh.sampledata.sample_geojson import geojson
+import psycopg2
+import time
 
 
 
@@ -21,43 +23,70 @@ def connect_to_postgres(connection):
 def write_master_db_to_postgres(master_db,table_name,engine):
     master_db.to_sql(table_name,engine)
 
-def append_group_sample(wave_id,group_id,engine):
+def create_full_sample_temp_table(wave_id,group_id,conn_db):
+    conn = conn_db.cursor() #create cursor to execute the direct db commands
+    temp_full_sample_table_name = "temp_wave_%d"%(wave_id) 
+    temp_table_name = "group_"+str(group_id)
+    sql_execution = """DROP TABLE IF EXISTS %s;"""%(temp_full_sample_table_name)
+    result = conn.execute(sql_execution)
+    sql_execution = """CREATE TEMP TABLE  %s (LIKE %s);"""%(temp_full_sample_table_name,temp_table_name)
+    print(sql_execution)
+    result = conn.execute(sql_execution)
+    conn.close()
+
+
+def append_group_sample(wave_id,group_id,conn_db):
+    conn = conn_db.cursor() #create cursor to execute the direct db commands
     temp_full_sample_table_name = "temp_wave_%d"%(wave_id)
     temp_table_name = "group_"+str(group_id)
-    sql_execution = """
-    CREATE TABLE IF NOT EXISTS %s (LIKE %s);
-    """%(temp_full_sample_table_name,temp_table_name)
+    time.sleep(1)
+    sql_execution = """insert into %s select * from %s;"""%(temp_full_sample_table_name,temp_table_name)
     print(sql_execution)
-    result = engine.execute(text(sql_execution).execution_options(autocommit=True))
-    sql_execution = """insert into %s select * from
-    %s;"""%(temp_full_sample_table_name,temp_table_name)
-    print(sql_execution)
-    result = engine.execute(text(sql_execution).execution_options(autocommit=True))
+    result = conn.execute(sql_execution)
+    conn.close()
+    return(result)
 
-
-def get_group_sample(group_id,engine,parent_table_name,population_filter,group_filter,group_value,samples):
+def get_group_sample(group_id,conn_db,parent_table_name,population_filter,group_filter,group_value,samples):
+    conn = conn_db.cursor() #create cursor to execute the direct db commands
+    over psycopg2
     temp_table_name = "group_"+str(group_id)
-    sql_execution = """DROP TABLE IF EXISTS %s;
-    CREATE TABLE %s as (select *,random() as random from %s where %s and %s in ('%s') order by random asc limit %d);"""%(temp_table_name,temp_table_name,parent_table_name,population_filter,group_filter,group_value,samples)
+    sql_execution = """DROP TABLE IF EXISTS %s;"""%(temp_table_name)
+    result = conn.execute(sql_execution)
+    sql_execution = """
+    CREATE TABLE %s as (
+            select *,random() as random 
+            from %s 
+            where %s 
+            and %s in ('%s') 
+            order by random asc 
+            limit %d);
+    """%(temp_table_name,parent_table_name,population_filter,group_filter,group_value,samples)
     print(sql_execution)
-    result = engine.execute(
-                    text(sql_execution).execution_options(autocommit=True))
+    result = conn.execute(sql_execution)
     sql_execution_count = """
             select count(*)
             from %s
-            where %s
-            and %s in ('%s');"""%(parent_table_name,population_filter,group_filter,group_value)
+            where %s 
+            and %s in ('%s');
+            """%(parent_table_name,population_filter,group_filter,group_value)
     print(sql_execution_count)
-    count = engine.execute(
-                text(sql_execution_count).execution_options(autocommit=True))
+    count = conn.execute(sql_execution_count)
     print(group_filter,group_value,count)
-    sample_count = pd.read_sql("""select count(*) from %s"""%(temp_table_name),
-                engine)
-    full_count = pd.read_sql("""select count(*) from %s where %s and %s in
-                ('%s')"""%(parent_table_name,population_filter,group_filter,group_value),
-                engine)
-    sample = pd.read_sql("""select * from %s"""%(temp_table_name),engine)
+    sample_count = pd.read_sql("""
+            select count(*) 
+            from %s;
+            """%(temp_table_name),conn_db)
+    full_count = pd.read_sql("""
+            select count(*) 
+            from %s 
+            where %s 
+            and %s in ('%s')
+            """%(parent_table_name,population_filter,group_filter,group_value),conn_db)
+    sample = pd.read_sql("""
+            select * from %s;
+            """%(temp_table_name),conn_db)
     print(sample_count['count'][0], samples)
+    conn.close()
     if sample_count['count'][0] < samples:
          # Create a new instance of an exception
          print('error')
@@ -87,9 +116,8 @@ def main():
     splitValues = ["afr_ZA","eng_ZA"]
     ###############
     
-    db_definitions = "postgresql://postgres:postgres@localhost:6432/postgres"
-    engine = connect_to_postgres(db_definitions)
-    
+    conn_db=psycopg2.connect(dbname=master_db_name,user=user,password=pwd,
+            port=database_port, host=database_server)
     population_filter = """
     Province in (
                     'Eastern Cape Province',
@@ -104,22 +132,24 @@ def main():
     group_values = ['afr_ZA','eng_ZA','xho_ZA']
     group_sample_number = [200,200,250]
     parent_table_name = "ecd_full_sample_data_set_deduped"
-    #for loop
-    group_value = group_values[i]
-    for i in range(0,len(group_values)):
-        (a,b,df) = get_group_sample(i,engine,parent_table_name,population_filter,group_filter,group_values[i],group_sample_number[i])
-        retVal = append_group_sample(wave,i,engine)
-        print(i)
     
+    #for loop
+    for i in range(0,len(group_values)):
+        (a,b,df) = get_group_sample(i,conn_db,parent_table_name,population_filter,group_filter,group_values[i],group_sample_number[i])
+        print(i)
+    #create the temp DB to hold all the samples together    
+    retVal = create_full_sample_temp_table(wave,0,conn_db)
+    
+    #append the individual samples into the new temp DB
+    for i in range(0,len(group_values)):
+        retVal = append_group_sample(wave,i,conn_db)
 
+    sample = pd.read_sql("""select * from temp_wave_1""",conn_db)
 
 
     output_file("stocks.html", title="stocks.py example")
-    sample = pd.read_sql("""select * from group_0""",engine)
     sample = engine.execute(text("""insert into temp_wave_1 select * from
         group_0""").execution_options(autocommit=true))
   count = engine.execute(
                           text(sql_execution_count).execution_options(autocommit=True))
-    sample = engine.execute(text("""drop table
-        temp_wave_1""").execution_options(autocommit=True))
 
